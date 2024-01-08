99cc3895ad347d8be55f5dd688ce8e3aa5b23d20
Róbert Klimant <64545490+RobertKlimant@users.noreply.github.com>
2024-01-09 00:15:50 +0100

    feat: nested cms by Niklas

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,34 @@
+import { loadElement } from '$utils/loadElement';
+import { queryElements } from '$utils/queryElements';
 import { swipers } from '$utils/swipers';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   swipers();
+
+  const instances: HTMLElement[] = queryElements('[nested-collection-element="target"]');
+  if (!instances.length) return;
+
+  instances.forEach((instance) => {
+    const nestTarget = instance;
+
+    const attributeValues = [
+      'nested-collection-item-slug',
+      'nested-collection-slug',
+      'nested-collection-class',
+    ].map((attribute) => nestTarget.getAttribute(attribute));
+
+    const [collectionItemSlug, collectionSlug, className] = attributeValues;
+    if (!collectionItemSlug || !collectionSlug || !className) return;
+
+    loadElement(nestTarget, collectionSlug, collectionItemSlug, className);
+
+    // const promise = new Promise((resolve, reject) => {
+    //   loadElement(nestTarget, collectionSlug, collectionItemSlug, className)
+    //     .then(() => resolve(true))
+    //     .catch((error) => reject(error));
+    // });
+
+    // promise.then(() => restartWebflow()).catch((error) => console.error(error));
+  });
 });
