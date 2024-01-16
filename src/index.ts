386669331176c94fc5eeb8333445052f2fbd7207
Róbert Klimant <64545490+RobertKlimant@users.noreply.github.com>
2024-01-16 14:11:56 +0100

    feat: clone buttons

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { cloneNode } from '@finsweet/ts-utils';
+
 import { nestedElement } from '$utils/nestElement';
 import { swipers } from '$utils/swipers';
 
@@ -8,4 +10,19 @@ window.Webflow.push(() => {
   swipers();
   nestedElement();
   modal();
+
+  const huvudmeny = document.querySelector('[menu-desktop="huvudmeny"]') as HTMLElement;
+  const huvudmenyButtons = huvudmeny.querySelectorAll('.button-primary') as NodeListOf<Element>;
+  const mobileHuvudEl = document.querySelector('[menu-mobile="huvudmeny"]') as HTMLElement;
+  huvudmenyButtons.forEach((button) => {
+    mobileHuvudEl.append(cloneNode(button));
+  });
+
+  const services = document.querySelector('[menu-desktop="tjanster"]') as HTMLElement;
+  const servicesButtons = services.querySelectorAll('.button-primary') as NodeListOf<Element>;
+  const mobileServiceEl = document.querySelector('[menu-mobile="tjanster"]') as HTMLElement;
+
+  servicesButtons.forEach((button) => {
+    mobileServiceEl.append(cloneNode(button));
+  });
 });
