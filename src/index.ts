4c8e2d876c2f02530f9197d0fe8d64e787cd5107
Róbert Klimant <roboklimant@gmail.com>
2024-01-17 01:50:11 +0100

    fix: navbar mobile

--- src/index.ts
+++ src/index.ts
@@ -11,15 +11,22 @@ window.Webflow.push(() => {
   nestedElement();
   modal();
 
+  const serviceAttr = document.querySelector('[service]')?.getAttribute('service');
+
   const huvudmeny = document.querySelector('[menu-desktop="huvudmeny"]') as HTMLElement;
-  const huvudmenyButtons = huvudmeny.querySelectorAll('.button-primary') as NodeListOf<Element>;
+  const huvudmenyCMS = huvudmeny.querySelector(
+    '[service-cms="' + serviceAttr + '"]'
+  ) as HTMLElement;
+  const huvudmenyButtons = huvudmenyCMS.querySelectorAll('.button-primary') as NodeListOf<Element>;
   const mobileHuvudEl = document.querySelector('[menu-mobile="huvudmeny"]') as HTMLElement;
+
   huvudmenyButtons.forEach((button) => {
     mobileHuvudEl.append(cloneNode(button));
   });
 
   const services = document.querySelector('[menu-desktop="tjanster"]') as HTMLElement;
-  const servicesButtons = services.querySelectorAll('.button-primary') as NodeListOf<Element>;
+  const serviceCMS = services.querySelector('[service-cms="' + serviceAttr + '"]') as HTMLElement;
+  const servicesButtons = serviceCMS.querySelectorAll('.button-primary') as NodeListOf<Element>;
   const mobileServiceEl = document.querySelector('[menu-mobile="tjanster"]') as HTMLElement;
 
   servicesButtons.forEach((button) => {
