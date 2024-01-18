39b039b66e087af7ddf991e2c42a6a356f6516cc
Róbert Klimant <roboklimant@gmail.com>
2024-01-18 14:36:49 +0100

    refactor: update imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { cloneNode } from '@finsweet/ts-utils';
-
+import { home } from '$utils/home';
+import { navbar } from '$utils/nav';
 import { nestedElement } from '$utils/nestElement';
 import { swipers } from '$utils/swipers';
 
@@ -10,26 +10,6 @@ window.Webflow.push(() => {
   swipers();
   nestedElement();
   modal();
-
-  const serviceAttr = document.querySelector('[service]')?.getAttribute('service');
-
-  const huvudmeny = document.querySelector('[menu-desktop="huvudmeny"]') as HTMLElement;
-  const huvudmenyCMS = huvudmeny.querySelector(
-    '[service-cms="' + serviceAttr + '"]'
-  ) as HTMLElement;
-  const huvudmenyButtons = huvudmenyCMS.querySelectorAll('.button-primary') as NodeListOf<Element>;
-  const mobileHuvudEl = document.querySelector('[menu-mobile="huvudmeny"]') as HTMLElement;
-
-  huvudmenyButtons.forEach((button) => {
-    mobileHuvudEl.append(cloneNode(button));
-  });
-
-  const services = document.querySelector('[menu-desktop="tjanster"]') as HTMLElement;
-  const serviceCMS = services.querySelector('[service-cms="' + serviceAttr + '"]') as HTMLElement;
-  const servicesButtons = serviceCMS.querySelectorAll('.button-primary') as NodeListOf<Element>;
-  const mobileServiceEl = document.querySelector('[menu-mobile="tjanster"]') as HTMLElement;
-
-  servicesButtons.forEach((button) => {
-    mobileServiceEl.append(cloneNode(button));
-  });
+  navbar();
+  home();
 });
